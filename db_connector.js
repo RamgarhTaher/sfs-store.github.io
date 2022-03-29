b7139c4aae82e5ef06d594923faf1ac4ebca4120
Taher Ramgarh <100428230+RamgarhTaher@users.noreply.github.com>
2022-03-29 15:50:04 +0530

    Update db_connector.js

--- db_connector.js
+++ db_connector.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const DB = `mongodb://localhost:27017/sfscollection`;
+const DB = `mongodb + srv://anyoneaccess:anyoneaccess@cluster0.hgrha.mongodb.net/sfsStoreDatabase?retryWrites=true&w=majority
+`;
 
 mongoose.connect(DB).then(() => {
     console.log('The server is sucsessfuly connected !');
